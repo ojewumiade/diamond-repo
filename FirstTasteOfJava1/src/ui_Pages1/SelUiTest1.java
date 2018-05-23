d1cccb5f29df5974e0f4566cf0f896634f139359
ojewumiade <ojewumiade@gmail.com>
2018-05-23 02:30:54 +0100

    added intelliJwork

---
+++ FirstTasteOfJava1/src/ui_Pages1/SelUiTest1.java
@@ -0,0 +1,12 @@
+package ui_Pages1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelUiTest1 {
+    public static void main(String[] args) {
+
+        System.setProperty ("webdriver.chrome.driver", "C:\\Users\\ojewu\\Documents\\Automation\\chromedriver");
+        WebDriver driver = new ChromeDriver();
+    }
+}
